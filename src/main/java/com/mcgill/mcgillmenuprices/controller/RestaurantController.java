4095f634e90055f3a855e519603778ad9715b0f0
wasifsomji <wasifsomji@gmail.com>
2024-03-21 18:07:34 -0400

    setting up service

--- src/main/java/com/mcgill/mcgillmenuprices/controller/RestaurantController.java
+++ src/main/java/com/mcgill/mcgillmenuprices/controller/RestaurantController.java
@@ -5,8 +5,10 @@ import com.mcgill.mcgillmenuprices.model.Restaurant;
 import com.mcgill.mcgillmenuprices.service.ItemService;
 import com.mcgill.mcgillmenuprices.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,5 @@ public class RestaurantController {
             RestaurantResponseDTO restaurant = restaurantService.getRestaurantByName(restaurantName);
             return ResponseEntity.ok(restaurant);
       }
+
 }
--- src/main/java/com/mcgill/mcgillmenuprices/service/CategoryService.java
+++ src/main/java/com/mcgill/mcgillmenuprices/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.mcgill.mcgillmenuprices.service;
 
+import com.mcgill.mcgillmenuprices.repository.CategoryRepository;
+import com.mcgill.mcgillmenuprices.repository.ItemRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoryService {
+      private CategoryRepository categoryRepository;
+      private ModelMapper modelMapper;
 }
